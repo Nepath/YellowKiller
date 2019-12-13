9f0617acddf36e47c443b6b5f813c792515e6f4d
dawbit <dawbitner@gmail.com>
2019-12-13 20:01:22 +0100

    added menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class MainMenuController : MonoBehaviour{
+
+    public GameObject MainMenu;
+    public GameObject Credits;
+
+    public void playGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+ 
+    public void options() {
+        
+    }
+	
+	public void creditcard() {
+		MainMenu.SetActive(false);
+        Credits.SetActive(true);     
+    }
+	
+	public void back() {
+		MainMenu.SetActive(true);
+		Credits.SetActive(false);
+    }
+ 
+    public void exitGame() {
+        Application.Quit();
+    }
+}
