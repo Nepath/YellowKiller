e8166cb6ec30f22bc70f5738940779665cc48ddd
MyisCARRY <tataauto@interia.pl>
2019-12-19 19:41:38 +0100

    Added hp, speed, jump force and weapon dmg

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,12 +8,14 @@ public class Weapon
     public float Accurancy;
     public int MagSize;
     public int FiringMode; //1 - single, 2 - shotgun, 3 - fullauto
+    public int Damage;
 
-    public Weapon(float firingRate, float accurancy, int magSize, int firingMode)
+    public Weapon(float firingRate, float accurancy, int magSize, int firingMode, int damage)
     {
         FiringRate = firingRate;
         Accurancy = accurancy;
         MagSize = magSize;
         FiringMode = firingMode;
+        Damage = damage;
     }
 }
